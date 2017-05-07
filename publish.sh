7289a4048aefc6e5f76a7c80165279263bcd1352
Karim Shehadeh <kshehadeh@underarmour.com>
2017-05-07 07:35:00 -0400

    Removed action endpoints since these are being handled by reports app.

--- publish.sh
+++ publish.sh
@@ -6,5 +6,14 @@ then
     ./bumpversion.sh
 fi
 
+# Make sure the readmes are the same
+echo "Copying README.md to README"
+cp README.md README
+
+# Update git
+echo "Updating in git"
+git commit -m "version bump and prep for publish"
+git push
+
 echo "Publishing version to python index..."
 python setup.py sdist upload -r local
